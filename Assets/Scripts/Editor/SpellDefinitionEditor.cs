f4b8682331de67d9a107ef56a58ff5c365300bb2
Owen Hey <oysterhey@gmail.com>
2023-12-24 00:21:36 -0500

    added custom inspector for spells
    - key / value system

---
+++ Assets/Scripts/Editor/SpellDefinitionEditor.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using Spells;
+
+[CustomEditor(typeof(SpellDefinition))]
+public class SpellDefinitionEditor : Editor
+{
+    private SpellDefinition _spellDefinition;
+    private SerializedProperty _attributesProperty;
+    private GUIStyle _centeredStyle;
+    private GUIStyle CenteredStyle
+    {
+        get
+        {
+            if (_centeredStyle == null)
+            {
+                _centeredStyle = new GUIStyle(GUI.skin.label)
+                {
+                    alignment = TextAnchor.MiddleCenter,
+                    fontSize = 12,
+                    fontStyle = FontStyle.Bold
+                };
+            }
+            return _centeredStyle;
+        }
+    }
+
+    private void OnEnable()
+    {
+        // Cache references
+        _spellDefinition = (SpellDefinition)target;
+        _attributesProperty = serializedObject.FindProperty("SpellAttributes");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        // Rest of the inspector
+        base.OnInspectorGUI();
+        
+        serializedObject.Update(); // Always call this at the beginning
+
+        if (_attributesProperty != null)
+        {
+            EditorGUILayout.Space(10);
+            EditorGUILayout.LabelField("Spell Attributes", CenteredStyle);
+            
+            if (GUILayout.Button("Add Attribute"))
+            {
+                _spellDefinition.SpellAttributes.Add(new SpellAttribute());
+            }
+            
+            for (int i = 0; i < _attributesProperty.arraySize; i++)
+            {
+                SerializedProperty attributeProperty = _attributesProperty.GetArrayElementAtIndex(i);
+                SerializedProperty keyProperty = attributeProperty.FindPropertyRelative("Key");
+                SerializedProperty valueProperty = attributeProperty.FindPropertyRelative("Value");
+
+                EditorGUILayout.BeginHorizontal();
+                
+                // Key Field
+                EditorGUILayout.PropertyField(keyProperty, GUIContent.none);
+
+                // Value Field
+                EditorGUILayout.PropertyField(valueProperty, GUIContent.none);
+
+                // Remove Button
+                if (GUILayout.Button("Remove"))
+                {
+                    _attributesProperty.DeleteArrayElementAtIndex(i);
+                }
+
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+
+        serializedObject.ApplyModifiedProperties(); // Apply changes at the end
+    }
+}
--- Assets/Scripts/Spells/SpellDefinition.cs
+++ Assets/Scripts/Spells/SpellDefinition.cs
@@ -1,6 +1,14 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Spells {
+    [System.Serializable]
+    public class SpellAttribute {
+        public string Key;
+        public float Value;
+    }
+    
     [CreateAssetMenu(fileName = "Spell", menuName = "Spell", order = 0)]
     public class SpellDefinition : ScriptableObject {
         public string EffectId;
@@ -10,5 +18,12 @@ namespace Spells {
         public Sprite SpellIcon;
         [Space(10)] 
         public SpellIndicatorData IndicatorData;
+
+        [HideInInspector] public List<SpellAttribute> SpellAttributes = new();
+        
+        public float GetAttributeValue(string key) {
+            var attribute = SpellAttributes.FirstOrDefault(a => a.Key == key);
+            return attribute != null ? attribute.Value : 0; // Return a default value if not found
+        }
     }
 }
